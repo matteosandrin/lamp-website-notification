d9212b3ac7348b9138194ccb19959d642c06f165
Matteo Sandrin <matteo.sandrin@hotmail.it>
2023-12-10 13:32:27 -0500

    Add timing functionality and add lamp check
     * Add functionality that times how long the light has been on for.
     * Add a check to make sure the lamp is on before we attempt to turn
       it on.

--- lamp-website-notification.py
+++ lamp-website-notification.py
@@ -1,4 +1,6 @@
 import json
+import math
+import time
 from python_pushover_open_client import PushoverOpenClientRealTime
 from python_pushover_open_client import PushoverOpenClient
 from python_pushover_open_client import CREDENTIALS_FILENAME
@@ -14,7 +16,6 @@ logging.info("Starting lamp-website-notification server...")
 
 LAMP_IP_ADDR: str = "10.0.0.53"
 LAMP_ON_DURATION: int = 30  # seconds
-turn_off_after_task = None
 
 
 def start_background_loop(loop: asyncio.AbstractEventLoop) -> None:
@@ -32,6 +33,8 @@ def setup_background_loop():
 pushover_credentials = json.load(open(CREDENTIALS_FILENAME))
 lamp = SmartPlug(LAMP_IP_ADDR)
 background_loop = setup_background_loop()
+turn_off_after_task = None
+turn_on_time = None
 
 
 def setup_pushover():
@@ -42,25 +45,40 @@ def setup_pushover():
     pushover_open_client.delete_all_messages()
 
 
+async def turn_on():
+    global lamp
+    global turn_on_time
+    await lamp.update()
+    if lamp.is_off:
+        logging.info(f"Turn ON lamp")
+        await lamp.turn_on()
+        turn_on_time = time.time()
+    else:
+        logging.info("The lamp is already ON")
+
+
 async def turn_off_after(seconds: int):
+    global lamp
+    global turn_on_time
+    logging.info(f"Schedule turn OFF in {seconds} seconds")
     await asyncio.sleep(seconds)
-    logging.info("Turn OFF lamp")
+    lamp_on_time = int(math.ceil(time.time() - turn_on_time))
+    logging.info(f"Turn OFF lamp. It was on for {lamp_on_time} seconds")
     await lamp.turn_off()
     logging.info("")
 
 
 @register_parser
 def personal_website_send_parser(raw_data=None):
+    global background_loop
     global turn_off_after_task
     if raw_data["message"].find("New personal website visit") != -1 and \
             raw_data["app"] == "Personal Website":
         logging.info("New personal website visit!")
-        logging.info(f"Turn ON lamp")
-        asyncio.run_coroutine_threadsafe(lamp.turn_on(), background_loop)
+        asyncio.run_coroutine_threadsafe(turn_on(), background_loop)
         if turn_off_after_task is not None and not turn_off_after_task.done():
-            logging.info("Canceling turn OFF task")
+            logging.info("Cancel turn OFF task")
             turn_off_after_task.cancel()
-        logging.info(f"Schedule turn OFF in {LAMP_ON_DURATION} seconds")
         turn_off_after_task = asyncio.run_coroutine_threadsafe(
             turn_off_after(LAMP_ON_DURATION), background_loop)
 
